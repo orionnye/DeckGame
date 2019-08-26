e1d0acf54a6decbb1e4d27530160331a3558cdf8
Kody King <kody.j.king@gmail.com>
2019-08-25 20:23:38 -0700

    more tweaks

--- src/CookBook.ts
+++ src/CookBook.ts
@@ -3,20 +3,28 @@ import CardTypes from "./CardTypes";
 
 class CookBook {
     static recipes: { [ name: string ]: CardType } = {}
+
     static getKey( ingredients: CardType[] ) {
-        return ingredients.map( c => c.name ).join( ", " )
+        return ingredients.map( c => c.name ).sort().join( "-" )
     }
+
     static addRecipe( product: CardType, ingredients: CardType[] ) {
         let key = CookBook.getKey( ingredients )
         CookBook.recipes[ key ] = product
     }
+
     static getProduct( ingredients: CardType[] ) {
         let key = CookBook.getKey( ingredients )
         return CookBook.recipes[ key ] || CardTypes.Volatile
     }
 }
 
-CookBook.addRecipe( CardTypes.Attack2, [ CardTypes.Attack1, CardTypes.Attack1 ] )
-CookBook.addRecipe( CardTypes.Heal2, [ CardTypes.Heal1, CardTypes.Heal1 ] )
+{
+    let { Attack1, Attack2, Heal1, Heal2, Volatile } = CardTypes
+    let add = CookBook.addRecipe
+    add( Attack2, [ Attack1, Attack1 ] )
+    add( Heal2, [ Heal1, Heal1 ] )
+    add( Volatile, [ Heal1, Attack1 ] )
+}
 
 export default CookBook
